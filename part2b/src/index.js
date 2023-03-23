23dca59aaa144ff5b1d7e59cf94ff09c644f9d94
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-03-23 11:34:10 -0400

    changes to index.js

--- part2b/src/index.js
+++ part2b/src/index.js
@@ -1,10 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import axios from 'axios'
 import App from './App'
 import './index.css'
 
-axios.get('http://localhost:3001/persons').then(res => {
-    const persons = res.data
-    ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons}/>)
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
