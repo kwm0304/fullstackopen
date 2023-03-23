8842982fbfe8c9947571bb7ec8821b451dfeac6b
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-03-23 18:44:33 -0400

    fixed 65000 eslint errors

---
+++ part3/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    // 'eqeqeq': 'error',
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
